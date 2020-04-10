bb66d8a3d1e364c646c9f59d358206a4725b1009
georgesavill <gpsavill+github@gmail.com>
2020-04-10 08:30:43 +0100

    Add JS for generating list of services

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,9 @@
 function populateServices(servicesJson) {
-    console.log(servicesJson);
+    for (var i = 0; i < servicesJson["service"].length; i++) {
+        var linkToService = document.createElement("a");
+        linkToService.innerHTML = servicesJson["service"][i]["name"];
+        linkToService.href = servicesJson["service"][i]["url"];
+        document.getElementById("content").appendChild(linkToService);
+    }
 }
-
 populateServices(jsonData);
